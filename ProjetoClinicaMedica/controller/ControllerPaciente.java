69cf8c69f9a8e64f006cc0e7f305331d7932c37a
lucasfsanti <47954905+lucasfsanti@users.noreply.github.com>
2019-06-05 09:51:34 -0300

    Começo de ControllerPaciente

--- ProjetoClinicaMedica/controller/ControllerPaciente.java
+++ ProjetoClinicaMedica/controller/ControllerPaciente.java
@@ -1,5 +1,71 @@
 package controller;
 
+import java.util.ArrayList;
+
+import model.bo.PacienteBO;
+import model.seletor.SeletorPaciente;
+import model.vo.PacienteVO;
+
 public class ControllerPaciente {
+
+	private PacienteBO pacienteBO = new PacienteBO();
+
+	public String salvarPaciente(PacienteVO paciente) {
+		String mensagem = validar(paciente);
+
+		if (mensagem == "") {
+			if (paciente.getIdPaciente() > 0) {
+				if (pacienteBO.atualizarPaciente(paciente)) {
+					mensagem += "Paciente atualizado com sucesso!";
+				} else {
+					mensagem += "Erro ao atualizar Paciente!";
+				}
+			} else {
+				if (pacienteBO.cadastrarPaciente(paciente)) {
+					mensagem += "Paciente cadastrado com sucesso!";
+				} else {
+					mensagem += "Erro ao cadastrar paciente!";
+				}
+			}
+		}
+		return mensagem;
+	}
+
+	public String excluirPaciente(PacienteVO paciente) {
+		String mensagem = "";
+		// TODO Implementar validação de ID
+		return mensagem;
+	}
+
+	public String validar(PacienteVO paciente) {
+		String mensagem = "";
+		if (paciente == null) {
+			mensagem += "Paciente está vazio";
+		}
+		if (paciente.getNome() == null || paciente.getNome().trim().isEmpty()) {
+			mensagem += "O campo NOME é obrigatório!\n";
+		}
+		if (paciente.getCpf() == null || paciente.getCpf().trim().isEmpty()) {
+			mensagem += "O campo CPF é obrigatório!\n";
+		}
+		if (paciente.getTelefone() == null || paciente.getTelefone().trim().isEmpty()) {
+			mensagem += "O campo TELEFONE é obrigatório!\n";
+		}
+		if (paciente.getTipoSanguineo() == null || paciente.getTipoSanguineo().trim().isEmpty()) {
+			mensagem += "O campo TIPO SANGUÍNEO é obrigatório!\n";
+		}
+		if (paciente.getSexo() == null || paciente.getSexo().trim().isEmpty()) {
+			mensagem += "O campo SEXO é obrigatório!\n";
+		}
+		if (paciente.getConvenio() == null || paciente.getConvenio().trim().isEmpty()) {
+			mensagem += "O campo CONVENIO é obrigatório!\n";
+		}
+
+		return mensagem;
+	}
+
+	public ArrayList<PacienteVO> pesquisarPaciente(SeletorPaciente seletor) {
+		return pacienteBO.pesquisarPaciente(seletor);
+	}
 
 }
--- ProjetoClinicaMedica/model/bo/PacienteBO.java
+++ ProjetoClinicaMedica/model/bo/PacienteBO.java
@@ -1,5 +1,25 @@
 package model.bo;
 
+import java.util.ArrayList;
+
+import model.seletor.SeletorPaciente;
+import model.vo.PacienteVO;
+
 public class PacienteBO {
+
+	public boolean atualizarPaciente(PacienteVO paciente) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean cadastrarPaciente(PacienteVO paciente) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public ArrayList<PacienteVO> pesquisarPaciente(SeletorPaciente seletor) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
