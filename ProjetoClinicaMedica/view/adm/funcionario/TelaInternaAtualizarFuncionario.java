6ff92c6564fd749a1012bd4831b75d0ee29a4ad9
lucasfsanti <47954905+lucasfsanti@users.noreply.github.com>
2019-06-24 19:59:00 -0300

    a

--- ProjetoClinicaMedica/view/adm/funcionario/TelaInternaAtualizarFuncionario.java
+++ ProjetoClinicaMedica/view/adm/funcionario/TelaInternaAtualizarFuncionario.java
@@ -12,11 +12,8 @@ import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
-import controller.ControllerConsulta;
-import controller.ControllerFuncionario;
 import net.miginfocom.swing.MigLayout;
 
 public class TelaInternaAtualizarFuncionario extends JInternalFrame {
@@ -33,7 +30,6 @@ public class TelaInternaAtualizarFuncionario extends JInternalFrame {
 	private JCheckBox chckbxMedico;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
 	private JFormattedTextField ftfNome;
-	
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -52,22 +48,23 @@ public class TelaInternaAtualizarFuncionario extends JInternalFrame {
 		super("Clínica Médica - Atualziar Dados dos Funcionarios (ADM)", false, true, false, false);
 		setDefaultCloseOperation(JInternalFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 1048, 773);
-		getContentPane().setLayout(new MigLayout("", "[10][60.00px,center][grow][grow][10][grow][10][grow][10]", "[5][30,fill][30.00px,fill][5][30,fill][40.00,fill][5][73.00,grow,fill][10][41.00,fill][10]"));
+		getContentPane().setLayout(new MigLayout("", "[10][60.00px,center][grow][grow][10][grow][10][grow][10]",
+				"[5][30,fill][30.00px,fill][5][30,fill][40.00,fill][5][73.00,grow,fill][10][41.00,fill][10]"));
 
 		initialize();
 	}
 
 	private void initialize() {
-		
+
 		JLabel lblFuncionario = new JLabel("Funcionario:");
 		getContentPane().add(lblFuncionario, "cell 1 1,alignx center,growy");
-		
+
 		JLabel lblNome = new JLabel("Nome:");
 		getContentPane().add(lblNome, "cell 1 2,alignx trailing,growy");
-		
+
 		JLabel lblCpf = new JLabel("CPF:");
 		getContentPane().add(lblCpf, "cell 1 4,alignx center,growy");
-		
+
 		chckbxFuncionario = new JCheckBox("Funcionarios");
 		getContentPane().add(chckbxFuncionario, "cell 2 1,alignx center,growy");
 		chckbxMedico = new JCheckBox("Médicos");
@@ -80,7 +77,7 @@ public class TelaInternaAtualizarFuncionario extends JInternalFrame {
 
 		ftfCpf = new JFormattedTextField();
 		getContentPane().add(ftfCpf, "cell 2 4 4 1,grow");
-		
+
 		btnPesquisar = new JButton("Pesquisar");
 		getContentPane().add(btnPesquisar, "cell 7 2 1 3,grow");
 		btnPesquisar.addActionListener(e -> {
@@ -94,19 +91,18 @@ public class TelaInternaAtualizarFuncionario extends JInternalFrame {
 			ftfCpf.setText("");
 		});
 
-		btnPesquisarTodos = new JButton("Pesquisar");
-		getContentPane().add(btnPesquisarTodos, "cell 5 5 3 1,grow");
-		btnPesquisarTodos.addActionListener(e -> {
-			
-			if (chckbxFuncionario.isSelected()) {
-				String nome = ftfNome.getText();
-				String cpf = ftfCpf.getText();
-				ControllerFuncionario controller = new ControllerFuncionario();
-				controller.ConsultarFuncionarioADM(nome, cpf);
-			}
+//		btnPesquisarTodos = new JButton("Pesquisar");
+//		getContentPane().add(btnPesquisarTodos, "cell 5 5 3 1,grow");
+//		btnPesquisarTodos.addActionListener(e -> {
 
-		});
+//			if (chckbxFuncionario.isSelected()) {
+//				String nome = ftfNome.getText();
+//				String cpf = ftfCpf.getText();
+//				ControllerFuncionario controller = new ControllerFuncionario();
+//				controller.ConsultarFuncionarioADM(nome, cpf);
+//			}
 
+//		});
 
 		btnSalvar = new JButton("Salvar Dados");
 		getContentPane().add(btnSalvar, "cell 3 9,grow");
@@ -121,21 +117,21 @@ public class TelaInternaAtualizarFuncionario extends JInternalFrame {
 			// TODO Pegar a linha/coluna selecionada e alterar diretamente na tabela
 			// int[] id = tableClientes.getSelectedRows();
 		});
-		
+
 		Object[][] data = new Object[][] { { "#", "Nome", "CPF", "DT Nascimento", "Telefone", "Celular", "E-Mail" }, };
 		Object[] columnNames = new String[] { "#", "Nome", "CPF", "DT Nascimento", "Telefone", "Celular", "E-Mail" };
-
 
 		table = new JTable();
 		getContentPane().add(table, "cell 1 7 7 1,grow");
 		table.setModel(new DefaultTableModel(data, columnNames));
-		
+
 		JCheckBox chkbApagarCliente = new JCheckBox("Apagar Clientes");
 		getContentPane().add(chkbApagarCliente, "cell 7 9,alignx leading,growy");
 		chkbApagarCliente.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (chkbApagarCliente.isSelected()) {
-					JOptionPane.showMessageDialog(null,	"Aten��o, Cuidado ao apagar um Funcionario!\nIsso pode gerar Problemas!");
+					JOptionPane.showMessageDialog(null,
+							"Aten��o, Cuidado ao apagar um Funcionario!\nIsso pode gerar Problemas!");
 					btnApagar.setEnabled(true);
 				}
 
